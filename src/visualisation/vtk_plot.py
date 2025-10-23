8e70a6918a6b73e69df576351e374016c2f88b66
Pyrrahh <jhjadv@t-online.de>
2025-10-23 14:09:59 +0200

    updated cplantbox to newest version

--- src/visualisation/vtk_plot.py
+++ src/visualisation/vtk_plot.py
@@ -4,8 +4,6 @@ from visualisation.vtk_tools import *
 import time
 import numpy as np
 import vtk
-from mpi4py import MPI; comm = MPI.COMM_WORLD; rank = comm.Get_rank(); max_rank = comm.Get_size()
-# from IPython.display import Image, display
 
 """
 VTK Plot, by Daniel Leitner (refurbished 06/2020)
@@ -20,7 +18,7 @@ def plot_leaf(leaf):
     """
     leaf_points = vtk.vtkPoints()
     leaf_polys = vtk.vtkCellArray()  # describing the leaf surface area
-    create_leaf(leaf, leaf_points, leaf_polys)
+    create_leaf_(leaf, leaf_points, leaf_polys)
     polyData = vtk.vtkPolyData()
     polyData.SetPoints(leaf_points)
     polyData.SetPolys(leaf_polys)
@@ -98,7 +96,7 @@ def plot_plant(plant, p_name, render = True, interactiveImage = True):
 
 
 def create_leaf_(leaf, leaf_points, leaf_polys):
-    """ used by plot plant """
+    """ used by plot plant, and cplantbox dash gui; adds to leaf_points, and leaf_polys """
     offs = leaf_points.GetNumberOfPoints()
     globalIdx_y = []  # index of y node
     for i in range(0, leaf.getNumberOfNodes() - 1):  #
@@ -330,8 +328,8 @@ def render_window(actor, title, scalarBar, bounds, interactiveImage = True):
         writer.SetInputConnection(windowToImageFilter.GetOutputPort())
         writer.Write()
 
-        # move this somewhere else?
-        im = Image(writer.GetResult(), format = "jpeg")
+        from IPython.display import Image, display
+        im = Image(writer.GetResult(), format = "jpeg")  # move this somewhere else?
         display(im)
 
 
@@ -641,6 +639,8 @@ def plot_plant_and_soil(rs, pname:str, rp, s, periodic:bool, min_b, max_b, cell_
     if sol_ind > 0:
         solute = np.array(s.getSolution(sol_ind))
 
+    from mpi4py import MPI;rank = comm.Get_rank();  # moved it here because it caused trouble for webapp server
+
     if rank == 0:
         if isinstance(rs, pb.SegmentAnalyser):
             ana = rs
@@ -682,7 +682,6 @@ def plot_plant_and_soil(rs, pname:str, rp, s, periodic:bool, min_b, max_b, cell_
             write_vtu(path + filename + ".vtu", soil_grid)
 
 
-
 def plot_roots_and_soil(rs, pname:str, rp, s, periodic:bool, min_b, max_b, cell_number, filename:str = "", sol_ind = 0, interactiveImage = True):
     """ Plots soil slices and roots, additionally saves both grids as files
     @param rs            some Organism (e.g. RootSystem, MappedRootSystem, ...) or MappedSegments
@@ -699,6 +698,9 @@ def plot_roots_and_soil(rs, pname:str, rp, s, periodic:bool, min_b, max_b, cell_
     pHead = np.array(s.getSolutionHead())
     if sol_ind > 0:
         solute = np.array(s.getSolution(sol_ind))
+
+    from mpi4py import MPI; rank = comm.Get_rank();  # moved it here because it caused trouble for webapp server
+
     if rank == 0:
         if isinstance(rs, pb.SegmentAnalyser):
             ana = rs
